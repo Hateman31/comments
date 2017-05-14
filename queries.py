e3f18d95da117d31dba0b25996a3bd31dfbc6dee
Hateman31 <metilogrunge31@gmail.com>
2017-05-14 12:01:21 +0300

    add queries text

--- queries.py
+++ queries.py
@@ -5,15 +5,29 @@ def addComment(cursor,values):
 		insert into comments (surname,name,patronim,email,phone,msg )
 		values (?,?,?,?,?)
 	'''
-	cursor.execute(query,*values)
+	cursor.execute(query,values['surname'],values['name'],values['patronim'],values['email'],values['phone'],values['msg'])
 	
-def delComment():
-	pass
+def delComment(cursor,id_):
+	query = '''
+		delete from comments
+		where id = ?
+	'''
+	cursor.execute(query,id_)
 
-def showCountByRegions():
-	pass
+def showCountByRegions(region = None):
+	query = '''
+		select *
+		from statByRegions
+		where name = ?
+	'''
+	return cursor.execute(query,region)
 
-def showCountByCities():
-	pass
-
-	
+def showCountByCities(r_code = None):
+	query = '''
+		select c.name,count(0)
+		from comments comm
+			on c.city_code = comm.city
+			and c.region_code = ?
+		group by c.name
+	'''
+	return cursor.execute(query,r_code)
--- start.sql
+++ start.sql
@@ -14,6 +14,8 @@ create table comments (
 	surname varchar,
 	name varchar,
 	patronim varchar,
+	region,
+	city,
 	email varchar,
 	phone int,
 	msg varchar(300)
@@ -26,3 +28,18 @@ insert into cities (city_code,region_code,name)
 values (1,1,'Краснодар'),(2,1,'Кропоткин'),(3,1,'Славянск'),
 	(4,2,'Ростов'),(5,2,'Шахты'),(6,2,'Батайск'),
 	(7,3,'Ставрополь'),(8,3,'Пятигорск'),(9,3,'Кисловодск')
+
+
+create view statByRegions
+as
+select r.id,r.name,foo.cnt
+from (
+	select region_code,count(0) cnt
+	from comments
+	group by region_code
+	having count(0) > 5
+) as foo
+join regions r
+		on r.id = foo.region_code
+
+
